d5d423c26fd0f05da71566e110bbde1e053cda30
Leonardo <leofsb78@gmail.com>
2024-05-02 11:02:30 -0300

    criando a funcao adicionar tarefa

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,23 @@ function validaCampo(){
   return valida;
 
 }
+
+// funcao que adiciona a tarefa
+
+function adicionarTarefa(){
+  //criando variavel que vai receber a tarefa
+  let linhas = document.getElementById("task")
+  if(validaCampo()){
+    alert("Preencha o campo com a tarefa")
+    else{
+      //adcionando as tarefas
+      tarefas.push(linha.value)
+      //passando para a proxima linha vazia
+      linhas.value="";
+      //chamando a funcao que vai mostrar todas as tarefas
+      listarTarefas();
+    }
+    //volta para o input da tarefa
+    document.getElementById("task").focus();
+  }
+}
